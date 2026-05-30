3fa0e0df041535a8c5f935349767a974ab8ea710
Iskandar <iyuldashev144@email.com>
2026-05-30 13:24:07 +0500

    Fix leave domain and repo

--- internal/domain/leave.go
+++ internal/domain/leave.go
@@ -22,11 +22,12 @@ const (
 type LeaveRequest struct {
 	ID         uuid.UUID   `db:"id" json:"id"`
 	EmployeeID uuid.UUID   `db:"employee_id" json:"employee_id"`
+	FullName   string      `db:"full_name" json:"full_name"`
 	LeaveType  LeaveType   `db:"leave_type" json:"leave_type"`
 	StartDate  time.Time   `db:"start_date" json:"start_date"`
 	EndDate    time.Time   `db:"end_date" json:"end_date"`
 	Reason     string      `db:"reason" json:"reason"`
 	Status     LeaveStatus `db:"status" json:"status"`
-	ReviewedBy *uuid.UUID  `db:"reviewed_by" json:"reviewed_by,omitempty"`
+	ReviewerID *uuid.UUID  `db:"reviewer_id" json:"reviewer_id,omitempty"`
 	CreatedAt  time.Time   `db:"created_at" json:"created_at"`
 }
